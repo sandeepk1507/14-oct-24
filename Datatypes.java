700a8137cf9418ee4a07366ffc1be83fe02b2aab
Sandeep Kaur <sandeep1260.be21@chitkara.edu.in>
2024-10-14 14:39:31 +0530

    first commit

---
+++ Datatypes.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Datatypes{
+public static void main(String[] args)
+{
+int a=5;
+float f = 68.965454f;
+char c = 'S';
+boolean b = true;
+if(b==true)
+{
+	System.out.println("true");
+}
+System.out.println(c);
+System.out.println(f);
+System.out.println(a);
+}
+}
