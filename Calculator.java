0c0df686bf592acf8b09af0759b5fd64f2252b97
Sandeep Kaur <sandeep1260.be21@chitkara.edu.in>
2024-10-16 09:57:07 +0530

    first commit

---
+++ Calculator.java
@@ -0,0 +1,39 @@
+import java.util.*;
+class Calculator { 
+   public static void main(String[] args) 
+    {
+      /*  int num1;
+        int num2;
+        String operation;
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("first number");
+        num1 = input.nextInt();
+
+        System.out.println("second number");
+        num2 = input.nextInt();
+
+
+        System.out.println("Please enter operation");
+        operation = op.next();
+
+        if (operation == "+");
+        {
+            System.out.println("your answer is" + (num1 + num2));
+        }
+        if  (operation == "-");
+        {
+            System.out.println("your answer is" + (num1 - num2));
+        }
+
+        if (operation == "/");
+        {
+            System.out.println("your answer is" + (num1 / num2));
+        }
+        if (operation == "*")
+        {
+            System.out.println("your answer is" + (num1 * num2));
+        }*/
+		System.out.print("Welcome");
+	}
+}
